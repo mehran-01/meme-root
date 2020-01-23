41f10ad06d623c80e0e0c5d41fb4b373e2290946
Mehran <mehran.hzh@gmail.com>
2020-01-22 17:41:02 -0800

    Updates

---
+++ precache-manifest.6168f1c1730588f980e6a171c34dc255.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2a4fb9a36ab8b7873631faf6ec22be",
+    "url": "/meme-root/index.html"
+  },
+  {
+    "revision": "bfabc0d1856ce9d28486",
+    "url": "/meme-root/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1b93bc21ba4132a7c648",
+    "url": "/meme-root/static/css/main.cd5e2c40.chunk.css"
+  },
+  {
+    "revision": "bfabc0d1856ce9d28486",
+    "url": "/meme-root/static/js/2.564f85d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/meme-root/static/js/2.564f85d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b93bc21ba4132a7c648",
+    "url": "/meme-root/static/js/main.6a829e9f.chunk.js"
+  },
+  {
+    "revision": "76ceaa49f0f10e6a28ee",
+    "url": "/meme-root/static/js/runtime-main.4ddfbb98.js"
+  },
+  {
+    "revision": "c4508356a4b9e54e893120c999cc92d0",
+    "url": "/meme-root/static/media/Goofy.c4508356.woff"
+  }
+]);
